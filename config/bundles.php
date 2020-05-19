679c3cbb30cf792ea10c811c0297d50847c19a2f
p3ters0ft <a.tiemeni@gmail.com>
2020-05-19 15:38:56 +0100

    [BO]Wisiwig sur des champs en BO
    Vous devez faire les actions suivantes:
    - composer update
    - php bin/console ckeditor:install
    -  php bin/console assets:install --symlink

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
